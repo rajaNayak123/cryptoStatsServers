771ff5e51be38cee70a22ccda8a79608b3a071db
rajaNayak123 <nayakraja151@gmail.com>
2025-05-18 07:32:02 +0530

    utility function for standard deviation calculatio

---
+++ api-server/src/utils/stdDev.js
@@ -0,0 +1,8 @@
+const stdDev = (values) => {
+  const mean = values.reduce((a, b) => a + b, 0) / values.length;
+  const variance =
+    values.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) /
+    values.length;
+  return Math.sqrt(variance);
+};
+module.exports = stdDev;
