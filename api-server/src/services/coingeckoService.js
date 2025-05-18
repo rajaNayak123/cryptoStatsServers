4c0450dccf16d655f3fd50b12db24287359fae0c
rajaNayak123 <nayakraja151@gmail.com>
2025-05-18 07:30:29 +0530

    cryptoService to fetch and store CoinGecko stats

---
+++ api-server/src/services/coingeckoService.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const storeCryptoStats = async () => {
+  const url = 'https://api.coingecko.com/api/v3/simple/price';
+  const coins = ['bitcoin', 'ethereum', 'matic-network'];
+  const params = {
+    ids: coins.join(','),
+    vs_currencies: 'usd',
+    include_market_cap: 'true',
+    include_24hr_change: 'true'
+  };
+  const { data } = await axios.get(url, { params });
+  const CryptoStat = require('../models/CryptoStat');
+  for (const coin of coins) {
+    const stat = new CryptoStat({
+      coin,
+      price: data[coin].usd,
+      marketCap: data[coin].usd_market_cap,
+      change24h: data[coin].usd_24h_change
+    });
+    await stat.save();
+  }
+};
+module.exports = { storeCryptoStats };
